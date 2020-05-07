9479e333414c379167e3637c0a230f0b3b07fb1e
Yuliaperegudova <yuse4ka08@gmail.com>
2020-05-07 07:46:07 +0300

    перенесет константу наверх

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,5 +1,7 @@
 import {eventTypes, CITIES, offersByEventsType} from "../const.js";
 
+const DESCRIPTION_SENTENCES_COUNT = 5;
+
 const getRandomIntegerNumber = (min, max) => {
   return min + Math.floor(Math.random() * (max - min));
 };
@@ -13,8 +15,6 @@ const getRandomArrayItem = (array) => {
 // описание нужно перенести в отдельный файл
 
 const destinationDescriptions = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
-
-const DESCRIPTION_SENTENCES_COUNT = 5;
 
 const generateDestinationDescription = () => {
   const descriptions = destinationDescriptions
